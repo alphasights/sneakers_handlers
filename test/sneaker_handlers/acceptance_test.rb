4aad64bac90e21e62e4f65102e45668dc0d71735
Brian Thomas Storti <btstorti@gmail.com>
2016-04-10 02:12:23 +0200

    Acceptance tests for dead letter handler

---
+++ test/sneaker_handlers/acceptance_test.rb
@@ -0,0 +1,62 @@
+require 'pry-byebug'
+require "test_helper"
+require "sneakers"
+require "sneakers/runner"
+require "rabbitmq/http/client"
+
+require_relative "workers/dead_letter_worker_failure"
+require_relative "workers/dead_letter_worker_success"
+
+class SneakersHandlers::AcceptanceTest < Minitest::Test
+
+  def test_dead_letter_messages
+    delete_test_queues!
+    configure_sneakers
+
+    pid = fork do
+      runner = Sneakers::Runner.new([DeadLetterWorkerFailure, DeadLetterWorkerSuccess])
+      runner.run
+    end
+
+    # Need some time to create the queues
+    sleep 0.1
+
+    exchange = channel.topic("sneakers_handlers", durable: false)
+
+    2.times do
+      exchange.publish("test message", routing_key: "sneakers_handlers.dead_letter_test")
+    end
+
+    sleep 0.1
+    success_dead_letter_queue = channel.queue(DeadLetterWorkerSuccess.queue_name + ".dlx")
+    failure_dead_letter_queue = channel.queue(DeadLetterWorkerFailure.queue_name + ".dlx")
+
+    assert_equal 2, failure_dead_letter_queue.message_count
+    assert_equal 0, success_dead_letter_queue.message_count
+
+    Process.kill("TERM", pid)
+  end
+
+  private
+
+  def channel
+    @channel ||= begin
+                   connection = Bunny.new.start
+                   connection.create_channel
+                 end
+  end
+
+  def delete_test_queues!
+    admin = RabbitMQ::HTTP::Client.new("http://127.0.0.1:15672/", username: "guest", password: "guest")
+    queues = admin.list_queues
+    queues.each do |q|
+      name = q.name
+      admin.delete_queue('/', name) if name.start_with?("sneaker_handlers")
+    end
+  end
+
+  def configure_sneakers
+    Sneakers.configure
+    Sneakers.logger.level = Logger::ERROR
+  end
+end
