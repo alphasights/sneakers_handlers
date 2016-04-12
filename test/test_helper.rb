0ccc3bdc330680ca5f15f0f88a2ad5e66d8002de
Abe Petrillo <abe.petrillo@gmail.com>
2016-04-12 14:28:57 -0400

    Remove dotenv dependency

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require "dotenv"
 require "sneakers_handlers"
 require "minitest/autorun"
 require "minitest/reporters"
 require "sneakers"
 
-Dotenv.load
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(color: true)
 
 Sneakers.configure
