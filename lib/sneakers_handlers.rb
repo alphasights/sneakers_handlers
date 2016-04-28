1b354b4ff7a519de0f5c9fe084b2ee3e6bc2f50c
John Bohn <jjbohn@gmail.com>
2016-04-28 10:23:38 -0400

    Add backoff handler to require

--- lib/sneakers_handlers.rb
+++ lib/sneakers_handlers.rb
@@ -1,7 +1,8 @@
 require "sneakers_handlers/version"
-require 'sneakers'
-require 'sneakers_handlers/dead_letter_handler'
-require 'sneakers_handlers/retry_handler'
+require "sneakers"
+require "sneakers_handlers/dead_letter_handler"
+require "sneakers_handlers/retry_handler"
+require "sneakers_handlers/exponential_backoff_handler"
 
 module SneakersHandlers
 end
